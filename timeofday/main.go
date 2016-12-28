6b720ae1dc9388782bf856a7a5814a708c2b3b11
Jurgen <jurgen@virtuos.com.cn>
2016-12-28 11:05:07 +0800

    -/+
    Added TimeOfDay main code
    Removed config folder and content

--- timeofday/main.go
+++ timeofday/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/jurgen-kluft/go-xbase"
 	"gopkg.in/redis.v5"
 	"io/ioutil"
 	"net/http"
@@ -16,11 +17,12 @@ func main() {
 	})
 
 	// Create and initialize
-	msgTimeOfDayConfigJSON, err := redisClient.Get("GO-HOME-TIMEOFDAY-CONFIG").Result()
+	ghTimeOfDayConfigJSON, err := redisClient.Get("GO-HOME-TIMEOFDAY-CONFIG").Result()
 	if err != nil {
 		panic(err)
 	}
-	timeOfDayConfig := CreateTimeOfDayConfig([]byte(msgTimeOfDayConfigJSON))
+	timeOfDayConfig := CreateTimeOfDayConfig([]byte(ghTimeOfDayConfigJSON))
+	fmt.Println(timeOfDayConfig)
 
 	// timeofday_config_key := "Go-Home-TimeOfDay-Config"
 	// timeofday_state_key := "Go-Home-TimeOfDay-State"
@@ -31,7 +33,13 @@ func main() {
 	sunsetSunriseResponse, _ := http.Get(timeOfDayConfig.URL)
 	sunsetSunriseConfigBytes, _ := ioutil.ReadAll(sunsetSunriseResponse.Body)
 	ssrc := CreateSunSetSunRiseConfig(sunsetSunriseConfigBytes)
-	fmt.Printf("SunSet: %v, SunRise: %v\n", ssrc.Results.Sunset, ssrc.Results.Sunrise)
+	sunset := &xbase.TimeOfDay{}
+	sunset.Parse(ssrc.Results.Sunset)
+	sunset.Add(&xbase.TimeOfDay{Hours: 8, Minutes: 0, Seconds: 0})
+	sunrise := &xbase.TimeOfDay{}
+	sunrise.Parse(ssrc.Results.Sunrise)
+	sunrise.Add(&xbase.TimeOfDay{Hours: 8, Minutes: 0, Seconds: 0})
+	fmt.Printf("Sunrise: %v, Sunset: %v", sunrise, sunset)
 
 	// channel_name := "Go-Home"
 
--- timeofday/timeofday.go
+++ timeofday/timeofday.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/jurgen-kluft/go-xbase"
 )
 
 // SHANGHAI :
@@ -48,6 +49,7 @@ type SunSetSunRiseConfig struct {
 
 // CreateSunSetSunRiseConfig returns an instance of SunSetSunRiseConfig by unmarshalling a stream of json bytes
 func CreateSunSetSunRiseConfig(jsondata []byte) (config *SunSetSunRiseConfig) {
+	config = &SunSetSunRiseConfig{}
 	json.Unmarshal(jsondata, config)
 	return
 }
@@ -59,54 +61,49 @@ func CreateSunSetSunRiseConfig(jsondata []byte) (config *SunSetSunRiseConfig) {
     "UpdateEvery": "300",
     "TimeOfDay": [
         {
-            "name": "SUN",
-            "start": "$(SUNRISE)",
-            "end": "$(SUNSET)"
-        },
-        {
             "name": "BREAKFAST",
-            "start": "7:00",
-            "end": "9:30"
+            "start": 7:00,
+            "end": 9:30
         },
         {
             "name": "MORNING",
-            "start": "6:00",
-            "end": "12:00"
+            "start": 6:00,
+            "end": 12:00
         },
         {
             "name": "NOON",
-            "start": "12:00",
-            "end": "13:00"
+            "start": 12:00,
+            "end": 13:00
         },
         {
             "name": "LUNCH",
-            "start": "11:45",
-            "end": "12:45"
+            "start": 11:45,
+            "end": 12:45
         },
         {
             "name": "AFTERNOON",
-            "start": "13:00",
-            "end": "18:00"
+            "start": 13:00,
+            "end": 18:00
         },
         {
             "name": "DINNER",
-            "start": "18:00",
-            "end": "20:00"
+            "start": 18:00,
+            "end": 20:00
         },
         {
             "name": "NIGHT",
-            "start": "20:00",
-            "end": "6:00"
+            "start": 20:00,
+            "end": 6:00
         },
         {
             "name": "SLEEPING",
-            "start": "22:00",
-            "end": "6:00"
+            "start": 22:00,
+            "end": 6:00
         },
         {
             "name": "EVENING",
-            "start": "$(SUN.start) - 0:30",
-            "end": "$(SLEEPING.start)"
+            "start": 16:30,
+            "end": 22:00
         }
     ]
 }
@@ -115,16 +112,23 @@ func CreateSunSetSunRiseConfig(jsondata []byte) (config *SunSetSunRiseConfig) {
 // TimeOfDayConfig contains the user configuration for naming certain periods of the day
 type TimeOfDayConfig struct {
 	URL         string `json:"URL"`
-	UpdateEvery string `json:"UpdateEvery"`
+	UpdateEvery int    `json:"UpdateEvery"`
 	TimeOfDay   []struct {
-		Name  string `json:"name"`
-		Start string `json:"start"`
-		End   string `json:"end"`
+		Name  string           `json:"name"`
+		Start *xbase.TimeOfDay `json:"start,string"`
+		End   *xbase.TimeOfDay `json:"end,string"`
 	} `json:"TimeOfDay"`
 }
 
 // CreateTimeOfDayConfig returns an instance of TimeOfDayConfig by unmarshalling a stream of json bytes
 func CreateTimeOfDayConfig(jsondata []byte) (config *TimeOfDayConfig) {
+	config = &TimeOfDayConfig{}
 	json.Unmarshal(jsondata, config)
 	return
+}
+
+func (t *TimeOfDayConfig) Find(timeofday *xbase.TimeOfDay) []string {
+	for _, v := range t.TimeOfDay {
+
+	}
 }
