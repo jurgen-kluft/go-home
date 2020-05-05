fb4200ac381b55b2505ded14eb20cc6a1719ff39
Jurgen <jurgen.kluft@gmail.com>
2020-05-05 17:39:40 +0800

    * revert to currantlabs/ble

--- ble-scan/main.go
+++ ble-scan/main.go
@@ -9,8 +9,8 @@ import (
 	"github.com/pkg/errors"
 	"golang.org/x/net/context"
 
-	"github.com/jurgen-kluft/ble"
-	"github.com/jurgen-kluft/ble/examples/lib/dev"
+	"github.com/currantlabs/ble"
+	"github.com/currantlabs/ble/examples/lib/dev"
 )
 
 var (
