140b35131b0dfb51e9992cf67cb3e06528fdf902
Jurgen Kluft <jurgen@virtuosgames.com>
2020-05-20 17:35:11 +0800

    * AQI: added example response

--- aqi/aqi_response.go
+++ aqi/aqi_response.go
@@ -17,6 +17,79 @@ func (r *CaqiResponse) marshal() ([]byte, error) {
 	return json.Marshal(r)
 }
 
+// Example response
+// {
+// 	"status": "ok",
+// 	"data": {
+// 	  "aqi": 87,
+// 	  "idx": 1437,
+// 	  "attributions": [
+// 		{
+// 		  "url": "http://106.37.208.233:20035/emcpublish/",
+// 		  "name": "China National Urban air quality real-time publishing platform (全国城市空气质量实时发布平台)"
+// 		},
+// 		{
+// 		  "url": "https://china.usembassy-china.org.cn/embassy-consulates/shanghai/air-quality-monitor-stateair/",
+// 		  "name": "U.S. Consulate Shanghai Air Quality Monitor"
+// 		},
+// 		{
+// 		  "url": "https://sthj.sh.gov.cn/",
+// 		  "name": "Shanghai Environment Monitoring Center(上海市环境监测中心)"
+// 		},
+// 		{
+// 		  "url": "https://waqi.info/",
+// 		  "name": "World Air Quality Index Project"
+// 		}
+// 	  ],
+// 	  "city": {
+// 		"geo": [
+// 		  31.2047372,
+// 		  121.4489017
+// 		],
+// 		"name": "Shanghai (上海)",
+// 		"url": "https://aqicn.org/city/shanghai"
+// 	  },
+// 	  "dominentpol": "pm25",
+// 	  "iaqi": {
+// 		"co": {
+// 		  "v": 5.5
+// 		},
+// 		"h": {
+// 		  "v": 21.3
+// 		},
+// 		"no2": {
+// 		  "v": 11.9
+// 		},
+// 		"p": {
+// 		  "v": 1007.7
+// 		},
+// 		"pm10": {
+// 		  "v": 57
+// 		},
+// 		"pm25": {
+// 		  "v": 87
+// 		},
+// 		"so2": {
+// 		  "v": 3.1
+// 		},
+// 		"t": {
+// 		  "v": 29.3
+// 		},
+// 		"w": {
+// 		  "v": 0.1
+// 		}
+// 	  },
+// 	  "time": {
+// 		"s": "2020-05-20 17:00:00",
+// 		"tz": "+08:00",
+// 		"v": 1589994000
+// 	  },
+// 	  "debug": {
+// 		"sync": "2020-05-20T18:29:53+09:00"
+// 	  }
+// 	}
+//}
+
 // CaqiResponse is the response we get from the HTTP request
 type CaqiResponse struct {
 	Status string `json:"status"`
